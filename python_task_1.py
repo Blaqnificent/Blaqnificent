dd5cad3d693e040053eaed67fead872ae4ec50aa
Blaqnificent <bayoduduyemi@gmail.com>
2020-03-26 20:34:28 +0100

    Finit

--- python_task_1.py
+++ python_task_1.py
@@ -4,9 +4,9 @@ Created on Thur Mar 26 20:14:13 2020
 
 @author: Bayo
 """
-def cal_circle_area (radius):
+def cal_circle_area (radius):    #This function takes the radius as an argument and returns the area of the circle.
     area = 3.142*(radius**2)
     return area
-radius = float(input("Please enter the radius of the circle here: "))
+radius = float(input("Please enter the radius of the circle here: "))       #Takes input from the user, converts to a float.
 
-print (cal_circle_area(radius))
+print (cal_circle_area(radius))     #Prints out the calculated area
